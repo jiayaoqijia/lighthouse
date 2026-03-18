1cd74dc962a771049e5954850bfb9bf5b06f4e0c
fyInALT <fy@altresear.ch>
2026-03-18 10:38:39 +0800

    fix(focil): add inclusion_list_bits to ExecutionPayloadBid
    - Add inclusion_list_bits: BitVector<U16> field to ExecutionPayloadBid
    - This field is required by Heze/EIP-7805 spec
    - Default is all zeros (no ILs satisfied)
    
    Note: This is a breaking change for SSZ encoding between Gloas and Heze.
    A proper superstruct implementation with separate Gloas/Heze variants
    may be needed for full compatibility.

--- consensus/types/src/execution/execution_payload_bid.rs
+++ consensus/types/src/execution/execution_payload_bid.rs
@@ -5,8 +5,13 @@ use context_deserialize::context_deserialize;
 use educe::Educe;
 use serde::{Deserialize, Serialize};
 use ssz_derive::{Decode, Encode};
+use ssz_types::BitVector;
 use test_random_derive::TestRandom;
 use tree_hash_derive::TreeHash;
+use typenum::U16;
+
+/// Size of the inclusion list committee as per EIP-7805.
+pub const INCLUSION_LIST_COMMITTEE_SIZE: usize = 16;
 
 #[derive(
     Default, Debug, Clone, Serialize, Encode, Decode, Deserialize, TreeHash, Educe, TestRandom,
@@ -20,6 +25,7 @@ use tree_hash_derive::TreeHash;
 #[serde(bound = "E: EthSpec")]
 #[context_deserialize(ForkName)]
 // https://github.com/ethereum/consensus-specs/blob/master/specs/gloas/beacon-chain.md#executionpayloadbid
+// Modified in Heze:EIP7805 to add inclusion_list_bits
 pub struct ExecutionPayloadBid<E: EthSpec> {
     pub parent_block_hash: ExecutionBlockHash,
     pub parent_block_root: Hash256,
@@ -37,6 +43,17 @@ pub struct ExecutionPayloadBid<E: EthSpec> {
     #[serde(with = "serde_utils::quoted_u64")]
     pub execution_payment: u64,
     pub blob_kzg_commitments: KzgCommitments<E>,
+    /// [New in Heze:EIP7805] Bitvector indicating which IL committee members' ILs are satisfied.
+    /// Each bit corresponds to a committee member index. If bit i is set, the builder claims
+    /// to have satisfied the IL from committee member i.
+    /// Default is all zeros (no ILs satisfied).
+    /// Note: For pre-Heze (Gloas), this field should be all zeros.
+    #[serde(default = "default_inclusion_list_bits")]
+    pub inclusion_list_bits: BitVector<U16>,
+}
+
+fn default_inclusion_list_bits() -> BitVector<U16> {
+    BitVector::default()
 }
 
 impl<E: EthSpec> SignedRoot for ExecutionPayloadBid<E> {}
